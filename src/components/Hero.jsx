f92589036c2a9dbca40850c4806e9a6294920bce
Samrat-Debugger <md721023@gmail.com>
2025-12-30 23:31:50 +0600

    hero responsive finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,23 +2,23 @@ import { FaPlayCircle, FaSquareFull } from "react-icons/fa";
 
 export default function Hero() {
   return (
-    <div className="grid grid-cols-2 items-center  bg-base-200 min-h-screen max-w-1xl pt-10 px-5 ">
+    <div className="grid lg:grid-cols-2 max-lg:gap-8 items-center  bg-base-200 min-h-screen max-w-1xl pt-10 px-5 ">
       <div className="">
         <h3 className="flex items-center gap-3 ">
-          <FaSquareFull className="text-rose-600 " /> Build Your Plan
+          <FaSquareFull className="text-rose-600" /> Build Your Plan
         </h3>
-        <h1 className="text-7xl font-bold ">
+        <h1 className="md:text-7xl xs:text-5xl text-3xl font-bold ">
           Everything you need to run your online business
         </h1>
-        <p className="py-6">
+        <p className="py-6 opacity-50">
           Get education on business planning, direct access to planning experts
           and a funding platform that’s helped raise over $500 million for
           startups and small businesses world-wide.
         </p>
-        <button className="btn btn-success btn-lg">
+        <button className="btn btn-success md:btn-lg ">
           Get Started <span className="opacity-50">-for free</span>
         </button>
-        <button className="btn btn-ghost btn-lg hover:bg-transparent hover:border-none hover:shadow-none hover:text-success">
+        <button className="btn btn-ghost md:btn-lg hover:bg-transparent hover:border-none hover:shadow-none hover:text-success">
           <FaPlayCircle /> Watch Demo
         </button>
       </div>
